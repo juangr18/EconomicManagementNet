3ff61ca788a908e7d6a1b0ac7ccddbaae4575a78
juangr18 <juanr970318@gmail.com>
2022-03-23 18:02:58 -0500

    Implement code for create users view

--- EconomicManagementAPP/Controllers/UsersController.cs
+++ EconomicManagementAPP/Controllers/UsersController.cs
@@ -1,41 +1,45 @@
-    using EconomicManagementAPP.Models;
-    using EconomicManagementAPP.Services;
-    using Microsoft.AspNetCore.Mvc;
+using EconomicManagementAPP.Models;
+using EconomicManagementAPP.Services;
+using Microsoft.AspNetCore.Mvc;
 
-    namespace EconomicManagementAPP.Controllers
+namespace EconomicManagementAPP.Controllers
+{
+    public class UsersController : Controller
     {
-        public class UsersController : Controller
+
+        private readonly IRepositorieUser repositorieUser;
+
+        public UsersController(IRepositorieUser repositorieUser)
         {
+            this.repositorieUser = repositorieUser;
+        }
 
-            private readonly IRepositorieUser repositorieUser;
+        public async Task<IActionResult> Index()
+        {
+            var users = await repositorieUser.getUsers();
 
-            public UsersController(IRepositorieUser repositorieUser)
+            return View(users);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(Users users)
+        {
+            if (!ModelState.IsValid)
             {
-                this.repositorieUser = repositorieUser;
-            }
-
-            public async Task<IActionResult> Index()
-            {
-                var users = await repositorieUser.getUsers();
-
                 return View(users);
             }
-            [HttpPost]
-            public async Task<IActionResult> Create(Users users)
+            var userExist = await repositorieUser.Exist(users.Email, users.Id);
+            if (userExist)
             {
-                if (!ModelState.IsValid)
-                {
-                    return View(users);
-                }
-                var userExist = await repositorieUser.Exist(users.Email, users.Id);
-                if (userExist)
-                {
-                    ModelState.AddModelError(nameof(users.Email),
-                        $"User with email {users.Email} already exist.");
-                    return View(users);
-                }
-                await repositorieUser.Create(users);
-                return RedirectToAction("Index");
+                ModelState.AddModelError(nameof(users.Email),
+                    $"User with email {users.Email} already exist.");
+                return View(users);
             }
+            await repositorieUser.Create(users);
+            return RedirectToAction("Index");
         }
     }
+}
