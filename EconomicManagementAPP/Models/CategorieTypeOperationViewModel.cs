2fe22e4a6d1df8e6c65f85cba300a23e52acf355
Jonathan <jonathan_huaripata94@outlook.com>
2022-03-28 17:11:05 -0500

    Categories update

---
+++ EconomicManagementAPP/Models/CategorieTypeOperationViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EconomicManagementAPP.Models
+{
+    public class CategorieTypeOperationViewModel : Categories
+    {
+        public IEnumerable<SelectListItem> OperationType { get; set; }
+    }
+}
